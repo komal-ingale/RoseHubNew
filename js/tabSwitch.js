d801777ce61d2422f30dbca1858d9a53f336b1d0
komal-ingale <kingale@uncc.edu>
2016-05-26 14:05:22 -0400

    On Refresh retain tab issue resolved

--- js/tabSwitch.js
+++ js/tabSwitch.js
@@ -2,7 +2,7 @@
 $(document).ready(function() {
     $('.tabs .tab-links a').on('click', function(e)  {
         var currentAttrValue = $(this).attr('href');
-
+        localStorage.setItem('activeTab', currentAttrValue);
         // Show/Hide Tabs
         $(currentAttrValue).addClass('active').siblings().removeClass('active');
         // Change/remove current tab to active
@@ -10,4 +10,12 @@ $(document).ready(function() {
 
         e.preventDefault();
     });
+    // on load of the page: switch to the currently selected tab
+    var hash =localStorage.getItem('activeTab');
+    if (hash != null) {
+        $(hash).addClass('active').siblings().removeClass('active');
+        // Change/remove current tab to active
+        var selector = "a[href=" + hash + "]";
+        $(selector).parent('li').addClass('active').siblings().removeClass('active');
+    }
 });
